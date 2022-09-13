17f557948890d1c7b393787f85aff15bee84e470
hvictor186 <hvictorleite@gmail.com>
2022-09-13 14:05:05 -0300

    Criada pasta 'Api', classe 'LivrosController' e método 'Recuperar', para CRUD de Livro.

--- Alura.WebAPI.WebApp/Controllers/LivroController.cs
+++ Alura.WebAPI.WebApp/Controllers/LivroController.cs
@@ -65,28 +65,6 @@ namespace Alura.ListaLeitura.WebApp.Controllers
             return View(model.ToModel());
         }
 
-        [HttpGet]
-        public IActionResult DetalhesSemHTML(int id)
-        {
-            var model = RecuperaLivro(id);
-            if (model == null)
-            {
-                return NotFound();
-            }
-            return Json(model.ToModel()); // Retorna objeto do tipo 'LivroUpload'.
-        }
-
-        [HttpGet]
-        public ActionResult<LivroUpload> DetalhesJson(int id)
-        {
-            var model = RecuperaLivro(id);
-            if (model == null)
-            {
-                return NotFound();
-            }
-            return model.ToModel(); // Retorna objeto do tipo 'LivroUpload'.
-        }
-
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Detalhes(LivroUpload model)
